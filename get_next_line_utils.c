e38be34d870900dd556f3d7110dc7ffebbd90dad
tinitiuset <tinitiuset@gmail.com>
2022-09-28 15:27:47 +0200

    Fixed another potential memory related bug

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: mvalient <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 09:37:26 by mvalient          #+#    #+#             */
-/*   Updated: 2022/09/26 23:13:50 by mvalient         ###   ########.fr       */
+/*   Updated: 2022/09/28 15:27:31 by mvalient         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,11 +59,15 @@ char	*ft_strjoin(char *s1, char *s2)
 {
 	int		i;
 	int		j;
+	int		len;
 	char	*ns;
 
 	if (!s1 && !s2)
 		return (NULL);
-	ns = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	len = ft_strlen(s1) + ft_strlen(s2);
+	if (!len)
+		return (NULL);
+	ns = malloc(len + 1);
 	if (!ns)
 		return (NULL);
 	i = -1;
